002fdd0b8d71408a39bbc856ef24b2883fdc0b2d
JessicaIsri <jessicajeidc@outlook.com>
2021-03-24 23:42:11 -0300

    Remoção do boot inicial do banco fora do liquibase

--- nemo/src/main/resources/db/changelog/schema.sql
+++ nemo/src/main/resources/db/changelog/schema.sql
@@ -1,8 +1,8 @@
-DROP TABLE IF EXISTS "candidate_skill";
-DROP TYPE IF EXISTS "skill_level";
-DROP TABLE  IF EXISTS "skill";
-DROP TABLE  IF EXISTS "candidate";
-DROP TABLE  IF EXISTS "client";
+-- DROP TABLE IF EXISTS "candidate_skill";
+-- DROP TYPE IF EXISTS "skill_level";
+-- DROP TABLE  IF EXISTS "skill";
+-- DROP TABLE  IF EXISTS "candidate";
+-- DROP TABLE  IF EXISTS "client";
 
 /* Table 'candidate' */
 CREATE TABLE "candidate" (
