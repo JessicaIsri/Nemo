ac7d4ede7f12a85b99ccfdcf42fd7f2f6fa846cc
felipemessi <felipemessibraga@gmail.com>
2021-06-08 19:01:10 -0300

    dataset fixed

--- nemo/src/main/resources/db/changelog/insert_selection_process.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO selection_process_status (status_name, fk_jo_id) VALUES('Pré-Selecionado', 1), ('Entrevista Marcada', 1),('Eliminado', 1),('Contratado', 1);
-
-INSERT INTO selection_process (fk_jo_id, fk_can_id, fk_status_name, obs) VALUES(1, 2, 'Pré-Selecionado', 'Fera'), (1, 1, 'Pré-Selecionado', 'Fera');
